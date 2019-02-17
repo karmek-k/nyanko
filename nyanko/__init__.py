f200d311a778f79eefacf40628462483285ad935
karmek-k <33288445+karmek-k@users.noreply.github.com>
2019-02-17 18:26:18 +0100

    add loading hosts from a file

--- nyanko/__init__.py
+++ nyanko/__init__.py
@@ -3,3 +3,10 @@ import nyanko.parsing
 
 
 hosts = None
+
+# load hosts from a file
+try:
+    with open(hosts) as fp:
+        hosts = tuple(line.strip() for line in fp)
+except FileNotFoundError:
+    print("Error: host file not found.")
